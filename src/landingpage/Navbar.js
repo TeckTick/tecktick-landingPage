fe90ae46488574e4259c211e67108911c04ae9b1
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2024-02-06 09:11:48 +0300

    Added google fonts i.e Inter and Roboto

--- src/landingpage/Navbar.js
+++ src/landingpage/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Button } from "@material-tailwind/react";
 
 const Navbar = () => {
   return (
-    <div>
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='header'>
+      <div className='logo'>
+        <img src='' alt='logo'/>
+      </div>
+      <div className='nav-menu'>
+        <a></a>
+        <a>About</a>
+        <a>Blog</a>
+        <a>Gallery</a>
+        <a>Sign In</a>
+        
+        <Button><a>Get Started</a></Button>
+     </div>
     </div>
   )
 }
