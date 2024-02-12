23219beb2a9084dcbbd472ba89208571f4b89059
beahnjoka <74968625+beahnjoka@users.noreply.github.com>
2024-02-12 09:25:43 +0300

    updated newsletter page

--- src/landingpage/Newsletter.js
+++ src/landingpage/Newsletter.js
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Newsletter = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
-  const subscribeHandler = () => {
-    console.log(`Subscribed with email: ${email}`);
-    setEmail('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here (e.g. send email to server)
   };
 
   return (
-    <div className="bg-gray-200 p-4 sm:p-8 rounded-md" style={{ backgroundColor: 'rgba(255, 255, 255, 1)' }}>
-      <h2 className="text-center text-2xl font-bold mb-4 text-[rgba(74, 74, 104, 1)]">Get Our Weekly Newsletter</h2>
-      <p className="mx-auto text-center text-gray-600 sm:w-96 mb-4">
-        Explanation of why you are going to love it and the benefit!
+    <div className="bg-[#FFFFFF] p-8 items-center h-[307px] g-[73px]">
+      <div className="text-[24px] font-montserrat text-center font-bold mb-4">Get Our Weekly Newsletter</div>
+      <p className="mb-10 text-center">
+      Explanation of why you are going to love it and the benefit!
       </p>
-      <div className="flex flex-col sm:flex-row">
+      <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row sm:items-center">
         <input
           type="email"
-          placeholder="Your email"
-          className="flex-1 p-2 border rounded-l-md mb-2 sm:mb-0 sm:mr-2"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          className="flex-1 p-2 border border-[#ECE4FF] rounded-[15.78px] mb-2 sm:mb-0 sm:mr-2"
+          required
         />
         <button
-          style={{ backgroundColor: 'rgba(69, 0, 153, 1)' }}
-          className="w-full sm:w-auto bg-blue-500 text-white px-4 py-2 rounded-r-md"
-          onClick={subscribeHandler}
+          type="submit"
+          className="bg-[#450099] text-white py-2 px-4 rounded-[15.78px] mb-[10px] w-full sm:w-auto"
         >
           Subscribe
         </button>
-      </div>
+      </form>
     </div>
   );
 };
