29c7e4fcad82b56ca676dc90f48532a8047edb4a
arnold-aswan <fredyaswan@gmail.com>
2024-02-20 16:10:29 +0300

    added about route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/shared/navbar/Navbar";
 import Footer from "./components/shared/footer/Footer";
 import Landingpage from "./pages/Landingpage";
-
+import About from "./pages/About";
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/tecktick-landingPage" element={<Landingpage />} />
-        
+        <Route path="/about-us" element={<About />} />
       </Routes>
       <Footer />
     </>
