3defd98b0a11fcc7e08a2a8d37b715b8d7a717b3
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-22 12:28:02 +0300

    add responsiveness to the about hero section

--- src/components/aboutpages/about-hero/Hero.js
+++ src/components/aboutpages/about-hero/Hero.js
@@ -1,21 +1,21 @@
 import React from "react";
-import about_hero from "../../../assets/images/about-hero.png";
+import about_hero from "../../../assets/images/about-hero.svg";
 import { GoChevronRight } from "react-icons/go";
 
 const Hero = () => {
- 
   return (
     <div
-      className="text-white w-full bg-black flex flex-col px-5 sm:px-10 md:px-20 font-montserrat pt-20"
+      className="text-white w-full flex flex-col px-5 sm:px-10 md:px-20 font-montserrat "
       style={{
         left: "calc(50% - 1440px/2)",
         top: "0px",
         background: `linear-gradient(0deg, rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)), url(${about_hero})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
       }}
     >
-      <span className="flex mt-5 text-sm gap-1 items-center justify-start">
+      <span className="flex text-sm gap-1 items-center justify-start mt-5">
         <p>Home</p>
         <p>
           <GoChevronRight />
@@ -23,7 +23,7 @@ const Hero = () => {
         <p>About us</p>
       </span>
 
-      <h1 className="text-center font-bold text-[32px] sm:text-[60px] max-w-[600px] self-center mt-20 mb-32">
+      <h1 className="text-center font-bold text-[24px] sm:text-[32px] md:text-[55px] lg:text-[60px] max-w-[600px] self-center mt-20 mb-32">
         Grow Better With Teck Tick
       </h1>
     </div>
