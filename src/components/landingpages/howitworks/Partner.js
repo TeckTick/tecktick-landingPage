630457b28050d1f87c754b1cf58ad444a08e1442
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-26 15:05:45 +0300

    fix partners

--- src/components/landingpages/howitworks/Partner.js
+++ src/components/landingpages/howitworks/Partner.js
@@ -2,14 +2,8 @@ const Partner = ({ id, img, name }) => {
   const image = img[name.toLowerCase()];
 
   return (
-    <div
-      key={id}
-      className="flex items-end justify-end h-[36px] mx-20"
-    >
-
-    Our Partners!
-      {/* <img src={image} alt={name} className="object-cover w-full h-full" /> */}
-
+    <div key={id} className="flex items-end justify-end h-[36px] mx-20">
+      <img src={image} alt={name} className="object-cover w-full h-full" />
     </div>
   );
 };
