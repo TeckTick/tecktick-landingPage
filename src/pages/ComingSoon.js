42ed31bd4272e90df3bebdf815353acc5549d76f
beahnjoka <74968625+beahnjoka@users.noreply.github.com>
2024-03-18 12:26:14 +0300

    coming soon

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -1,8 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react';
-import logo from '../assets/images/Logo.svg';
+// import logo from '../assets/images/Logo.svg';
 import maintainimg from '../assets/images/comingsoon.png';
 import { SocialIcon } from 'react-social-icons';
-import sendButton from '../assets/images/sendButton.png'
+import sendButton from '../assets/images/sendButton.png';
 
 const ComingSoon = () => {
   const [timerDays, setTimerDays] = useState('00');
@@ -46,74 +46,60 @@ const ComingSoon = () => {
   });
 
   return (
-    <div className="container mx-auto px-4 h-[800px] bg-[#FFFFFF]">
+    <div className="container mx-auto px-4 py-6 mt-4 md:py-12 lg:py-16 xl:py-20 bg-[#FFFFFF]">
       <div className="nav flex justify-between items-center py-4">
-        <img src={logo} alt="logo" className='text-[#4B6EE5]' />
+        {/* <img src={logo} alt="logo" className='text-[#4B6EE5]' /> */}
         <button className="bg-blue-500 text-white px-4 py-2 rounded">Contact</button>
-      </div>
+      </div> 
 
-      <div className="flex items-center">
-        <div className="ml-10 pr-20 w-[615px] h-[424px] left-[99px] top-10">
-          <img src={maintainimg} alt="maintainimg" className="" />
+      <div className="flex flex-col md:flex-row items-center">
+        <div className="md:ml-10 pr-4 md:pr-20 w-full md:w-[615px] h-[424px]">
+          <img src={maintainimg} alt="maintainimg" className="w-full h-auto" />
         </div>
-        <div className="text-start w-[556px] h-[317px]">
-          <div className='w-[556px] h-[112px] left-[770px]'>
-            <h1 className="font-normal text-4xl text-[#0f70eb] mb-4">We're Coming Soon</h1>
+        <div className="text-center md:text-start w-full md:w-[556px] h-auto md:h-[317px]">
+          <div className='mb-4'>
+            <h1 className="font-normal text-4xl text-[#0f70eb]">We're Coming Soon</h1>
             <p className="font-normal text-base text-[#77838f]">
               Our Portal is currently under construction. We'll be here soon with
               our new awesome site. Subscribe to be notified
             </p>
           </div>
-          <div className="flex items-center w-[484px] h-[73px] top-[407px]">
-            <section className="mr-2 w-[62px] h-[73px] top-[407px] left-[770px]">
+          <div className="flex justify-center md:justify-start items-center flex-wrap mb-4">
+            <div className="mr-4 md:mr-6">
               <p className='font-normal text-3xl text-[#1E2022]'>{timerDays}</p>
-              <p className='font-normal text-base text-[#77838F]'>
-                <small>Days</small>
-              </p>
-            </section>
-           
-            <section className="mr-2 w-[53px] h-[73px] top-[407px] left-[908px]">
+              <p className='font-normal text-base text-[#77838F]'><small>Days</small></p>
+            </div>
+            <div className="mr-4 md:mr-6">
               <p className='font-normal text-3xl text-[#1E2022]'>{timerHours}</p>
-              <p className='font-normal text-base text-[#77838F]'>
-                <small>Hours</small>
-              </p>
-            </section>
-           
-            <section className="mr-2 w-[53px] h-[73px] top-[407px] left-[1046px]">
+              <p className='font-normal text-base text-[#77838F]'><small>Hours</small></p>
+            </div>
+            <div className="mr-4 md:mr-6">
               <p className='font-normal text-3xl text-[#1E2022]'>{timerMinutes}</p>
-              <p className='font-normal text-base text-[#77838F]'>
-                <small>Minutes</small>
-              </p>
-            </section>
-           
-            <section className='mr-2 w-[53px] h-[73px] top-[407px] left-[1184px]'>
+              <p className='font-normal text-base text-[#77838F]'><small>Minutes</small></p>
+            </div>
+            <div>
               <p className='font-normal text-3xl text-[#1E2022]'>{timerSeconds}</p>
-              <p className='font-normal text-base text-[#77838F]'>
-                <small>Seconds</small>
-              </p>
-            </section>
+              <p className='font-normal text-base text-[#77838F]'><small>Seconds</small></p>
+            </div>
           </div>
-          <div className='flex'>
-  <input
-    type="email"
-    placeholder="Enter Your Email Address"
-    className="border border-gray-300 rounded px-4 py-2 mt-4 w-[555px]"
-  />
-  <button className='left-[565px] bg-[#743FE5] w-10 h-10 mt-4'>
-    <img src={sendButton} className='font-normal text-xl text-[#FFFFFF]' />
-  </button>
-</div>
-
-        </div>
-      </div>
-
-      <div className="Mfooter flex justify-between items-center py-4 w-[1262px] h-[19px] top-[743px] left-16">
-        <p className='font-normal text-sm text-[#77838F]'>&copy; TeckTick 2024</p>
-        <div className="social-media flex w-[104.06px] h-[16px] top-1 left-[1157.94px] gap-2">
-          <SocialIcon url="www.twitter.com"  className=' text-base font-normal '/>
-          <SocialIcon url="www.linkedin.com" className='text-base font-normal'/>
-          <SocialIcon url="www.google.com" className='text-base font-normal'/>
-          <SocialIcon url="www.github.com" className='text-base font-normal'/>
+          <div className='flex justify-center md:justify-start mb-4'>
+            <input
+              type="email"
+              placeholder="Enter Your Email Address"
+              className="border border-gray-300 rounded px-4 py-2 mb-4 md:mb-0 md:mr-4 w-full md:w-[555px]"
+            />
+            <button className='bg-[#743FE5] w-10 h-10'>
+              <img src={sendButton} alt="send" className='w-6 h-6  text-white' />
+            </button>
+          </div>
+          <div className="flex justify-between items-center md:py-4 md:top-[743px] md:left-16">
+            <div className="social-media flex gap-2 mb-4 md:mb-0">
+              <SocialIcon url="www.twitter.com" className='text-base font-normal' />
+              <SocialIcon url="www.linkedin.com" className='text-base font-normal' />
+              <SocialIcon url="www.google.com" className='text-base font-normal' />
+              <SocialIcon url="www.github.com" className='text-base font-normal' />
+            </div>
+          </div>
         </div>
       </div>
     </div>
